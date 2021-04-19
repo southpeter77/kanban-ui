c9f7490b0043e8ba5ca4991cd4ba4033a01c64bd
Peter <southpeter77@gmail.com>
2021-04-18 21:47:32 -0400

    splash page

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React from 'react';
+import Authentication from "../authentication/Authentication"
+import "./splash.css"
 
-
-const Splash = () => {
-
+function Splash() {
     return (
-        <div>
-            splash Page
+        <>
+        <div className="splash__intro">
+        <h1> welcome to kanban board</h1>
+        <h2>Let's make your work visible so you can show it to others and keep everyone on the same page</h2>
         </div>
-    )
+            
+            <Authentication/>
+            <h3>Peter Kang's Pensieve Frontend Coding Challenge</h3>
+        </>
+    );
 }
 
-
-export default Splash
+export default Splash;
