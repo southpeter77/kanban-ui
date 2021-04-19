f467f680d68815c1853ea55e3723ab6d9656f5c2
Peter <southpeter77@gmail.com>
2021-04-18 22:45:44 -0400

    user reducer created

--- src/components/splash/Splash.js
+++ src/components/splash/Splash.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import Authentication from "../authentication/Authentication"
-import "./splash.css"
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    headLine1: {
+        fontSize: "100px",
+        color: "rgb(214, 214, 214)",
+        textAlign: "center"
+    },
+    subHeadLine1: {
+        fontSize: "40px",
+        color: "rgb(214, 214, 214)",
+        textAlign: "center",
+        maxWidth: "60%",
+        margin: "auto auto"
+    },
+    subHeadLine2: {
+        fontSize: "30px",
+        color: "rgb(214, 214, 214)",
+        textAlign: "center",
+        maxWidth: "60%",
+        margin: "auto auto"
+    }
+
+}));
 
 function Splash() {
+    const classes = useStyles();
+
     return (
         <>
         <div className="splash__intro">
-        <h1> welcome to kanban board</h1>
-        <h2>Let's make your work visible so you can show it to others and keep everyone on the same page</h2>
+        <Typography component="h1" className={classes.headLine1}> welcome to kanban board</Typography>
+        <Typography component="h2" className={classes.subHeadLine1}>Let's make your work visible so you can show it to others and keep everyone on the same page</Typography>
         </div>
             
             <Authentication/>
-            <h3>Peter Kang's Pensieve Frontend Coding Challenge</h3>
+            <Typography component="h3" className={classes.subHeadLine2}>Peter Kang's Pensieve Frontend Coding Challenge</Typography>
         </>
     );
 }
